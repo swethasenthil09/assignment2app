609eb02c72b74935b0303f2f2d8bc995bdc842f6
swethasenthil09 <swethasenthil.0910@gmail.com>
2025-06-14 20:51:36 +0530

    analytics2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
-// pages/_app.js
-import "@/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
